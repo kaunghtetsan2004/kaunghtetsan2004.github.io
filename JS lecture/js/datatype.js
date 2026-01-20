b8be5efaaf89272fb4d775c5e81f658c0bf3be16
Kaung Htet <kaunghtetsan200422@gmail.com>
2026-01-20 19:53:31 +0630

    Upload

---
+++ JS lecture/js/datatype.js
@@ -0,0 +1,23 @@
+// String data type
+let name = "Noname";
+console.log(name);
+console.log(typeof (name));
+
+// numrical data type
+let age = 21;
+console.log(age);
+console.log(typeof (age));
+
+// boolean data type
+let single = true;
+console.log(single);
+console.log(typeof (single));
+
+// indified data type
+let myBaby;
+console.log(typeof (myBaby));
+
+// null
+let son = null;
+console.log(son);
+console.log(typeof (son));
